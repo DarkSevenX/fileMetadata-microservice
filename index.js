5b50d795bb2028163b3bae7b9c4056cacd7d160e
Glitch (mica-encouraging-pediatrician) <none>
2024-08-13 18:08:39 +0000

    🚏🙋 Checkpoint ./index.js:68957500/1231 ./views/index.html:68957500/1839

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+
+// require and use "multer"...
 
 var app = express();
 
@@ -8,13 +11,24 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
 });
 
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
 });
+
+let multer = require('multer')
+
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response) => {
+  let responseObject = {}
+  responseObject['name'] = request.file.originalname
+  responseObject['type'] = request.file.mimetype
+  responseObject['size'] = request.file.size
+  
+  response.json(responseObject)
+} )
